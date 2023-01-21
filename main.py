a1de190f8d3a420326ab52a233a91328a925d7e2
69NoraV <ragiofra2005@gmail.com>
2023-01-21 17:04:14 +0100

    Added to do list on readme; initialized pygame

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("im testing github repos")
+import pygame
+
+pygame.init()
+
+winsize = (1024, 576)
+
+screen = pygame.display.set_mode(winsize)
+pygame.display.set_caption("2048")
+
+main = True
+while main:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            main = False
+
+pygame.QUIT
