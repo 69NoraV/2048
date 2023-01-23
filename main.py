315a3895b6a8aec27f727bf91fa980208c5055d0
69NoraV <ragiofra2005@gmail.com>
2023-01-23 09:40:53 +0100

    now numbers show on screen

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ winsize = (400, 500)
 screen = pygame.display.set_mode(winsize)
 gridGUI = pygame.image.load("assets\\Grid.png").convert()
 font = pygame.font.SysFont("Impact", 35)
+numbersFont = pygame.font.SysFont("Impact", 40)
 
 #window name
 pygame.display.set_caption("2048")
@@ -38,6 +39,11 @@ def ShowMoves(x, y):
     movesTEXT = font.render("Moves: " + str(moves), 1, (63.2,15.1,0))
     screen.blit(movesTEXT, (x, y))
 
+#print numbers on gui (wip)
+def printNumberGUI(x,y):
+    number = numbersFont.render(str(2), 1, (63.2,15.1,0))
+    screen.blit(number, (x, y))
+
 #update value of grid 
 def UpdateValueGrid():
     for i in range(100):
@@ -45,6 +51,7 @@ def UpdateValueGrid():
         if grid[temp[0], temp[1]] == 0:
             grid[temp[0], temp[1]] = 2
             break
+    print(grid)
 
 #get input player
 def getInput():
@@ -59,9 +66,16 @@ def getInput():
     
     return 0
 
-#move numbers on GUI (WIP)
-def moveNumbers():
+#move numbers
+def moveNumberCode():
     pass
+
+#move numbers on GUI 
+def moveNumbersGUI():
+    for x in range(4):
+        for y in range(4):
+            if grid[[x],[y]] != 0:
+                printNumberGUI(100 * (y + 1) - 50, (100 * (x + 1) - 50) + 100 )
 
 #show grid 
 def ShowGUI(x, y):
@@ -73,6 +87,7 @@ def updateScreen():
     screen.fill((0,0,0))
     ShowGUI(0, 100)
     ShowMoves(10,30)
+    moveNumbersGUI()
 
     pygame.display.update()
     #lock framerate to 60
@@ -90,7 +105,6 @@ while main:
         #if the event is quit then quit
         if event.type == pygame.QUIT:
             main = False
-        getInput()
         if getInput() != 0:
             UpdateValueGrid()
             UpdateMoves()
